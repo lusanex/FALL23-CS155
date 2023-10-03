88fd5150ba62438026ac6b0b04c0e636b9bc40f4
lusanex <gutierrezerich3@gmail.com>
2023-10-02 18:20:36 -0700

    added hw5 part2

---
+++ Homework5_Part2/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = GenerateNumbers();
+Reverse(numbers);
+PrintNumbers(numbers);
+       
+
+static int[] GenerateNumbers()
+{
+    int[] numbers = new int[10];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = i + 1;
+    }
+    return numbers;
+}
+
+static void PrintNumbers(int[] numbers)
+{
+    foreach (int num in numbers)
+    {
+        Console.Write(num + " ");
+    }
+    Console.WriteLine();
+}
+
+static void Reverse(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length / 2; i++)
+    {
+        int temp = numbers[i];
+        numbers[i] = numbers[numbers.Length - i - 1];
+        numbers[numbers.Length - i - 1] = temp;
+    }
+}
---
+++ Homework5_Part2/readme.md
@@ -0,0 +1,1 @@
+![screenshot1](./pic.png)
